24d6f3f815d26de10fd36cf6a487086355d54ceb
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2021-06-30 01:52:32 +0300

    first commit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./intro.scss"
+
+const Intro = () => {
+    return (
+        <div className="intro" id="intro">
+            
+        </div>
+    );
+}
+
+export default Intro;
+
+
---
+++ src/components/works/Works.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./work.scss"
+
+const Works = () => {
+    return (
+        <div className="works" id="works">
+            
+        </div>
+    )
+}
+
+export default Works
